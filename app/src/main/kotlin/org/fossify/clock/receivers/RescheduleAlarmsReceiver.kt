ae59b0c6cc3f8889b4c5cdaa2a22eaaaa4d199ae
Naveen Singh <snaveen935@gmail.com>
2025-04-01 00:18:51 +0530

    Reschedule alarms when time or zone changes

--- app/src/main/kotlin/org/fossify/clock/receivers/RescheduleAlarmsReceiver.kt
+++ app/src/main/kotlin/org/fossify/clock/receivers/RescheduleAlarmsReceiver.kt
@@ -1,13 +1,18 @@
 package org.fossify.clock.receivers
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import org.fossify.clock.extensions.alarmController
 import org.fossify.clock.extensions.goAsync
 
+/**
+ * Receiver responsible for rescheduling alarms in background.
+ */
 class RescheduleAlarmsReceiver : BroadcastReceiver() {
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
         goAsync {
             context.alarmController.rescheduleEnabledAlarms()
